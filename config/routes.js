dbf99af144859688d881c4f6cdb7cc16a3938798
Peter Williams <pedroeldiablo@yahoo.com>
2016-11-16 15:49:36 +0000

    user routes added

--- config/routes.js
+++ config/routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth');
 const pinsController = require('../controllers/pins');
+const usersController = require('../controller/users');
 
 router
   .post('/login', authController.login)
@@ -14,5 +15,13 @@ router.route('/pins/:id')
   .get(pinsController.show)
   .put(pinsController.update)
   .delete(pinsController.delete);
+
+router.route('/users')
+  .get(usersController.index);
+
+router.route('/users/:id')
+  .get(usersController.show)
+  .put(usersController.update)
+  .delete(usersController.delete);
 
 module.exports = router;
